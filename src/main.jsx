176cbf34cb2194d29811f1a8215b30dc986fb0a0
Mateus Vieira <habdig7@gmail.com>
2023-05-04 19:28:49 -0300

    ENV CSS

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //import './index.css' 
 
+import "./assets/CSS/reset.css"
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./assets/CSS/style.css"
 
 //import bootstrap from "bootstrap";
 
