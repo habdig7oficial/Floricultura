9e1c6b0486108582c288f54afd52c6179d8206c8
Mateus Felipe Vieira <habdig7@gmail.com>
2023-05-05 17:07:58 -0300

    AGPL boilerplate added

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,25 @@
+/*
+    Este arquivo é parte de Flor de Lis
+
+    Copyright © 2023 Heloísa Real, Thiago Baptista, Mateus Vieira
+
+    Este programa é um software livre; você pode redistribuí-lo e/ou
+    modificá-lo sob os termos da Licença Pública Geral GNU como publicada
+    pela Free Software Foundation; na versão 3 da Licença, ou
+    (a seu critério) qualquer versão posterior.
+
+    Este programa é distribuído na esperança de que possa ser útil,
+    mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+    a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+    Licença Pública Geral GNU para mais detalhes.
+
+    Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+    com este programa. Se não, veja <http://www.gnu.org/licenses/>.
+
+    Licença disponível no arquivo COPYING.txt
+    
+*/
+
 import Navbar from "./components/_NavBar";
 import Card from "./components/_Card";
 import Footer from "./components/_Footer";
